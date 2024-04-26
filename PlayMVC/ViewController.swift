dcba63ea859558ce323f0bddffbb628cf263732a
kwazzar <fugitiwe@gmail.com>
2024-04-26 12:27:54 +0300

    Initial Commit

---
+++ PlayMVC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlayMVC
+//
+//  Created by Quasar on 26.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
